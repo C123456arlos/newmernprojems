a51e0207b4f4093230b6530d5c8d37bf4bd27d96
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-09-09 19:05:26 -0400

    first commit

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+      <div className='flex h-screen bg-iinear-to-br from-slate-50 via-white to-indigo-50/30'>
+          <p>sidebar</p>
+          <main className='flex-s overflow-y-auto'>
+              <div className='p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-400 mx-auto'>
+                  <Outlet></Outlet>
+              </div>
+      </main>
+      </div>
+  )
+}
+
+export default Layout
