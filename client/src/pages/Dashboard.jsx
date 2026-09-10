66abbf5ef5e338ac1ba0a845294d6f124d85b3ea
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-09-10 08:55:41 -0400

    first commit

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react"
+import { dummyAdminDashboardData, dummyEmployeeDashboardData } from "../assets/assets"
+import Loading from "../components/Loading"
+import EmployeeDashboard from "../components/EmployeeDashboard"
+import AdminDashboard from "../components/AdminDashboard"
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setData(dummyAdminDashboardData)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, []
   )
+  if (loading) return <Loading></Loading>
+  if(!data) return <p className="text-center text-slate-500 py-12">failed to load dashboard</p>
+  if (data.role === 'ADMIN') {
+    return <AdminDashboard data={data}></AdminDashboard>
+  } else {
+    return <EmployeeDashboard data={data}></EmployeeDashboard>
+  }
 }
 
 export default Dashboard
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -6,7 +6,9 @@ const Layout = () => {
   return (
       <div className='flex h-screen bg-iinear-to-br from-slate-50 via-white to-indigo-50/30'>
           <Sidebar></Sidebar>
-          <main className='flex-s overflow-y-auto'>
+          <main className=' w-full overflow-y-auto'>
+          {/* <main className='flex overflow-y-auto items-center w-full'> */}
+          {/* <main className='flex-s overflow-y-auto items-center'> */}
               <div className='p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-400 mx-auto'>
                   <Outlet></Outlet>
               </div>
